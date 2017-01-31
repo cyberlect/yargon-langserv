7c8cd1fa1c26ce7aa5b38ed31254e73da837d42b
Daniel Pelsmaeker <info@virtlink.com>
2017-01-31 11:18:15 +0100

    Moved Yargon.JsonRpc to yargon-jsonrpc repository

--- test/Yargon.JsonRpc.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yargon.JsonRpc.Tests")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("f2bf81fd-0be5-4170-a74b-d84463b8dbd9")]
